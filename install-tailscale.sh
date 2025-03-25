54223c769ac13934f32bdbaedd34b0dd393eea9f
ChAlanA <88502758+ChalanaGimhanaX@users.noreply.github.com>
2025-03-26 00:31:15 +0530

    Update install-tailscale.sh

--- install-tailscale.sh
+++ install-tailscale.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -fsSL https://tailscale.com/install.sh | sh && sudo tailscale up --auth-key=tskey-auth-kHuXFw9H6q11CNTRL-vKMoXE9YMA9Hxd4ivrHTA9kvWdkpmEoSK
+curl -fsSL https://tailscale.com/install.sh | sh && sudo tailscale up --auth-key=tskey-auth-kFrYwTLBJK11CNTRL-2H4zbKdiMV83s4yoZKwFV8NLYExaAaZb
